f2de0ead432f51546964af623e55ec654a05dea2
zeldner26 <misha@10.100.102.83>
2025-06-01 15:40:03 +0300

    הכנסנו את התוכן של הגוי של הטרמינל ועדכננו את התוכן של הכפתורים ושאר הדברים לקונטרולר

--- BparkClientSide/src/client/TerminalController.java
+++ BparkClientSide/src/client/TerminalController.java
@@ -1,16 +1,52 @@
 package client;
 
+import javafx.scene.control.TextArea;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
 /**
  * Controller for the terminal interface.
  */
 public class TerminalController implements BaseController {
  
     private ChatClient client;
+    
+    @FXML private Button btndropoff, btnpickup, btnexit, btnbackinsertparkingcode, btnbackdropoff, btnLoginFirstTer, 
+           btnViewFirstParkingSpot,btnFirstBack;
+    
+    @FXML private Label labelchooseservice, labelpickupcar, labeldropoddcar, labelnoparkingavailable, labelinsetparkingcode;
+    
+    @FXML private TextField textfieldparkingcode;
+    
+    @FXML private TextArea textfareadisplaypcode;
+    
+    @FXML private Hyperlink hyperlinkforgetpass;
+    
+    @FXML private Text textleavecaronconveyor, text4hoursanddelay;
+    
+    
 
     @Override
-    public void setClient(ChatClient client) {
+    public void setClient(ChatClient client) 
+    {
         this.client = client;
     }
+    
+    
+    @FXML
+    private void handleExit() 
+    {
+        if (client != null) {
+            client.quit();
+        } else {
+            System.exit(0);
+        }
+    }
+    
 
     // TODO: Add @FXML elements and logic
 }
